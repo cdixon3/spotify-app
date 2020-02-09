cfa77dab23712e928381ce84889b4dd55be39ad7
Christopher Dixon <christopher.dixon@oracle.com>
2020-02-08 17:52:01 -0700

    added button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
 import "./App.css";
 import ReactRevealText from "react-reveal-text";
+import { AwesomeButton } from "react-awesome-button";
+// import "react-awesome-button/dist/styles.css";
+import "react-awesome-button/src/styles/themes/theme-flat/styles.scss";
 class Wrapper extends React.Component {
   constructor() {
     super();
-    this.state = { show: false };
+    this.state = { show: false, buttonShow: false };
   }
 
   componentDidMount() {
     setTimeout(() => {
       this.setState({ show: true });
-    }, 300);
+    }, 200);
+    setTimeout(() => {
+      this.setState({ buttonShow: true });
+    }, 3000);
   }
 
   render() {
@@ -18,6 +24,11 @@ class Wrapper extends React.Component {
       <div className="upper-background">
         <div className="text-styles">
           <ReactRevealText show={this.state.show} text="MAKE IT UNCOMMON" />
+        </div>
+        <div className="button-styles">
+          <AwesomeButton type="primary" visible={this.state.buttonShow}>
+            GET STARTED
+          </AwesomeButton>
         </div>
       </div>
     );
